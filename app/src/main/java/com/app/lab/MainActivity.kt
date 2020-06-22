4a18bff8cd6da33d6bd69e1b88bfe770c79deddf
Pablo de Cárcer <pdecarcer@abstract.cl>
2020-06-22 19:00:13 -0400

    Recovery lab templeate

---
+++ app/src/main/java/com/app/lab/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.app.lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.root_layout, ..... , "MainFragment")
+                .commit()
+    }
+}
